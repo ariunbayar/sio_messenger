3f3ea131323c51a1510de90475dcf8a24ea7d484
Ariunbayar B <1480970+ariunbayar@users.noreply.github.com>
2019-11-08 18:14:51 +0800

    Button for creating new thread

---
+++ static/js/messenger.js
@@ -0,0 +1,11 @@
+(function(){
+
+
+    let btnNew = document.querySelector('.create-thread');
+
+    btnNew.addEventListener('click', function(){
+        console.log('TODO create new thread');
+    });
+
+
+})();
