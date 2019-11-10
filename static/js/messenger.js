16ecc5b4b542baed162fd782991d43f8b4d90c5d
Ariunbayar B <1480970+ariunbayar@users.noreply.github.com>
2019-11-10 17:14:48 +0800

    Select styling for current thread

--- static/js/messenger.js
+++ static/js/messenger.js
@@ -1,43 +1,64 @@
-function InitThread(thread_id){
-    var loc = window.location
-    var formData = $("#form")
-    var msgInput = $('#chatmessage')
-    var chatHolder = $("#chat-messages")
+(function(){
 
-    var wsStart = 'ws://'
+    let threadItems = document.querySelectorAll('.messenger > .thread-list > ul.thread-list > li');
+
+    function activateThreadItem(threadItems, threadItem) {
+        threadItems.forEach((item) => item.classList.remove('active'));
+        threadItem.classList.add('active');
+    }
+
+    threadItems.forEach((threadItem) => {
+
+        threadItem.addEventListener('click', (e) => {
+            activateThreadItem(threadItems, threadItem);
+            let thread_id = threadItem.getAttribute('data-thread-id');
+            initThread(thread_id);
+        });
+
+    });
+
+
+    let loc = window.location;
+    let formData = $("#form");
+    let msgInput = $('#chatmessage');
+    let chatHolder = $("#chat-messages");
+    let wsStart = 'ws://'
     if (loc.protocol == 'https'){
         wsStart = 'wss://'
     }
 
-    var endpoint = wsStart + loc.host + '/' + thread_id + '/'
-    console.log(endpoint)
-    var socket = new ReconnectingWebSocket(endpoint)
+    function initThread(thread_id){
 
-    socket.onmessage = function(e){
-        var chatDataMsg = JSON.parse(e.data)
-        chatHolder.append('<div class="message">' +
-        '    <div class="time">12:00</div>' +
-        '    <div class="user">' + chatDataMsg.username + ':</div>' +
-        '    <div class="message">'+ chatDataMsg.message +'</div>' +
-        '</div>')
-    }
-    socket.onopen = function(e){
-        formData.submit(function(event){
-            event.preventDefault()
-            var msgText = msgInput.val()
-            var finalData = {
-                'message': msgText
-            }
+        var endpoint = wsStart + loc.host + '/' + thread_id + '/'
+        var socket = new ReconnectingWebSocket(endpoint)
 
-            socket.send(JSON.stringify(finalData))
-            msgInput.val('')
-            formData[0].reset()
-        })
+        socket.onmessage = function(e){
+            var chatDataMsg = JSON.parse(e.data)
+            chatHolder.append('<div class="message">' +
+            '    <div class="time">12:00</div>' +
+            '    <div class="user">' + chatDataMsg.username + ':</div>' +
+            '    <div class="message">'+ chatDataMsg.message +'</div>' +
+            '</div>')
+        }
+        socket.onopen = function(e){
+            formData.submit(function(event){
+                event.preventDefault()
+                var msgText = msgInput.val()
+                var finalData = {
+                    'message': msgText
+                }
+
+                socket.send(JSON.stringify(finalData))
+                msgInput.val('')
+                formData[0].reset()
+            })
+        }
+        socket.onerror = function(e){
+            console.log("error", e)
+        }
+        socket.onclose = function(e){
+            console.log("close", e)
+        }
     }
-    socket.onerror = function(e){
-        console.log("error", e)
-    }
-    socket.onclose = function(e){
-        console.log("close", e)
-    }
-}
+
+})();
