d761e4958e793a9a70c5b2a7d764370657190b8d
Ariunbayar B <1480970+ariunbayar@users.noreply.github.com>
2019-11-10 17:01:18 +0800

    Styling for current thread. Removes unused code

--- static/js/messenger.js
+++ static/js/messenger.js
@@ -1,44 +1,43 @@
 function InitThread(thread_id){
-	var loc = window.location
-	var formData = $("#form")
-	var msgInput = $('#chatmessage')
-	var chatHolder = $("#chat-messages")
-	var me = $("#myUsername").val()
+    var loc = window.location
+    var formData = $("#form")
+    var msgInput = $('#chatmessage')
+    var chatHolder = $("#chat-messages")
 
-	var wsStart = 'ws://'
-	if (loc.protocol == 'https'){
-	    wsStart = 'wss://'
-	}
+    var wsStart = 'ws://'
+    if (loc.protocol == 'https'){
+        wsStart = 'wss://'
+    }
 
-	var endpoint = wsStart + loc.host + '/' + thread_id + '/'
-	console.log(endpoint)
-	var socket = new ReconnectingWebSocket(endpoint)
+    var endpoint = wsStart + loc.host + '/' + thread_id + '/'
+    console.log(endpoint)
+    var socket = new ReconnectingWebSocket(endpoint)
 
-	socket.onmessage = function(e){
-	    var chatDataMsg = JSON.parse(e.data)
-	    chatHolder.append('<div class="message">' +
+    socket.onmessage = function(e){
+        var chatDataMsg = JSON.parse(e.data)
+        chatHolder.append('<div class="message">' +
         '    <div class="time">12:00</div>' +
         '    <div class="user">' + chatDataMsg.username + ':</div>' +
         '    <div class="message">'+ chatDataMsg.message +'</div>' +
         '</div>')
-	}
-	socket.onopen = function(e){
-	    formData.submit(function(event){
-	        event.preventDefault()
-	        var msgText = msgInput.val()
-	        var finalData = {
-	            'message': msgText
-	        }
+    }
+    socket.onopen = function(e){
+        formData.submit(function(event){
+            event.preventDefault()
+            var msgText = msgInput.val()
+            var finalData = {
+                'message': msgText
+            }
 
-	        socket.send(JSON.stringify(finalData))
-	        msgInput.val('')
-	        formData[0].reset()
-	    })
-	}
-	socket.onerror = function(e){
-	    console.log("error", e)
-	}
-	socket.onclose = function(e){
-	    console.log("close", e)
-	}	
+            socket.send(JSON.stringify(finalData))
+            msgInput.val('')
+            formData[0].reset()
+        })
+    }
+    socket.onerror = function(e){
+        console.log("error", e)
+    }
+    socket.onclose = function(e){
+        console.log("close", e)
+    }
 }
